8e2608fb0128c5fc467555748e9e98b136edc417
dmn1k <dominik.schlosser@gmail.com>
2018-04-12 15:54:11 +0200

    Initialer Stand (Spring boot projekt und Dockerfile)

---
+++ src/main/java/com/github/dmn1k/wbhprojekt/WbhProjektApplication.java
@@ -0,0 +1,12 @@
+package com.github.dmn1k.wbhprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbhProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbhProjektApplication.class, args);
+	}
+}
