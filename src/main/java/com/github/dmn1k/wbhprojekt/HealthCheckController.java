d3517ad469e8b42e56289289a5bdd6b959ff52aa
dmn1k <dominik.schlosser@gmail.com>
2018-04-13 15:04:48 +0200

    Refactoring

--- src/main/java/com/github/dmn1k/wbhprojekt/HealthCheckController.java
+++ src/main/java/com/github/dmn1k/wbhprojekt/HealthCheckController.java
@@ -1,7 +1,5 @@
 package com.github.dmn1k.wbhprojekt;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/health")
 public class HealthCheckController {
-    private static final Logger LOGGER = LoggerFactory.getLogger(HealthCheckController.class);
     @GetMapping
     public ResponseEntity<?> health(){
-        LOGGER.info("healthcheck yeah");
         return ResponseEntity.ok("healthy");
     }
 }
