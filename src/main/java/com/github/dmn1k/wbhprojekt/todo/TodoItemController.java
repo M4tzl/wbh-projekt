55d41c447bb0ed2e04118d2a28d523fd6934dfee
dmn1k <dominik.schlosser@gmail.com>
2018-04-14 00:33:35 +0200

    Build-Timestamp wird jetzt angezeigt

--- src/main/java/com/github/dmn1k/wbhprojekt/todo/TodoItemController.java
+++ src/main/java/com/github/dmn1k/wbhprojekt/todo/TodoItemController.java
@@ -1,6 +1,7 @@
 package com.github.dmn1k.wbhprojekt.todo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +13,12 @@ public class TodoItemController {
     @Autowired
     private TodoItemRepository todoRepo;
 
+    @Value("${build.timestamp}")
+    private String buildTimestamp;
+
     @GetMapping("/")
     public String index(Model model){
+        model.addAttribute("buildTimestamp", buildTimestamp);
         model.addAttribute("items", todoRepo.findAll());
         model.addAttribute("newItem", new TodoItem());
         return "index";
