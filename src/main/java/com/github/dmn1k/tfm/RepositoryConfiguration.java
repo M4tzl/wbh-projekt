4aaccf8a575a5dcab1ce17a7023c52e8ebf9f4b2
dmn1k <dominik.schlosser@gmail.com>
2018-09-02 13:00:15 +0200

    expose ids

---
+++ src/main/java/com/github/dmn1k/tfm/RepositoryConfiguration.java
@@ -0,0 +1,17 @@
+package com.github.dmn1k.tfm;
+
+import com.github.dmn1k.tfm.inserate.Inserat;
+import com.github.dmn1k.tfm.inserate.Story;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+@Configuration
+public class RepositoryConfiguration extends RepositoryRestConfigurerAdapter {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Inserat.class);
+        config.exposeIdsFor(Story.class);
+    }
+}
