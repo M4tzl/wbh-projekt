65a3bbc4b93655df71822a1514bb7a0b11effa06
dmn1k <dominik.schlosser@gmail.com>
2018-09-02 18:31:00 +0200

    Layout

--- src/main/angular/app/components/inserate/detail/inserate-detail.component.ts
+++ src/main/angular/app/components/inserate/detail/inserate-detail.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from "@angular/core";
 import {InserateService} from "../../../services/inserate.service";
 import {ActivatedRoute} from "@angular/router";
 import {Inserat} from "../../../model/inserat";
+import {Location} from '@angular/common';
 
 @Component({
     selector: 'app-inserate-detail',
@@ -12,6 +13,7 @@ export class InserateDetailComponent implements OnInit {
     inserat: Inserat;
 
     constructor(private inserateService: InserateService,
+                private location: Location,
                 private route: ActivatedRoute) {
     }
 
@@ -20,5 +22,9 @@ export class InserateDetailComponent implements OnInit {
         const id = this.route.snapshot.params['id'];
         this.inserateService.load(id)
             .subscribe(result => this.inserat = result);
+    }
+
+    goBack(): void {
+        this.location.back();
     }
 }
