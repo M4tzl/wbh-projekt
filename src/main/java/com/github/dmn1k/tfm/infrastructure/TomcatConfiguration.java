7c0ef2c95989d24424cb783ee2692b07c6f39357
dmn1k <dominik.schlosser@gmail.com>
2018-09-11 08:48:46 +0200

    read ssl keyfile from s3

--- src/main/java/com/github/dmn1k/tfm/infrastructure/TomcatConfiguration.java
+++ src/main/java/com/github/dmn1k/tfm/infrastructure/TomcatConfiguration.java
@@ -1,14 +1,24 @@
 package com.github.dmn1k.tfm.infrastructure;
 
+import com.github.dmn1k.tfm.files.FileService;
+import lombok.Cleanup;
+import lombok.SneakyThrows;
 import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 @Configuration
 public class TomcatConfiguration {
@@ -18,9 +28,13 @@ public class TomcatConfiguration {
     @Value("${server.port}")
     private int serverPortHttps;
 
-    @Value("${server.ssl.enabled}")
+    @Value("${ssl.required}")
     private boolean sslRequired;
 
+    @Autowired
+    private FileService fileService;
+
+    @SneakyThrows
     @Bean
     public ServletWebServerFactory servletContainer() {
         if (sslRequired) {
@@ -36,12 +50,35 @@ public class TomcatConfiguration {
                 }
             };
 
-            tomcat.addAdditionalTomcatConnectors(redirectConnector());
+            File privateKeyFile = readPrivateKeyFile();
 
+            tomcat.addAdditionalTomcatConnectors(redirectConnector());
+            tomcat.addConnectorCustomizers(con -> configureTomcatKeystore(con, privateKeyFile));
             return tomcat;
         }
 
         return new TomcatServletWebServerFactory();
+    }
+
+    private void configureTomcatKeystore(Connector con, File privateKeyFile) {
+        Http11NioProtocol proto = (Http11NioProtocol) con.getProtocolHandler();
+        proto.setSSLEnabled(true);
+        con.setScheme("https");
+        con.setSecure(true);
+        proto.setKeystoreFile(privateKeyFile.getAbsolutePath());
+        proto.setKeystorePass("tfm-keystore");
+        proto.setKeystoreType("PKCS12");
+        proto.setProperty("keystoreProvider", "SunJSSE");
+        proto.setKeyAlias("tfm");
+    }
+
+    private File readPrivateKeyFile() throws IOException {
+        ResponseEntity<byte[]> keyFile = fileService.download("tfm.p12");
+        File tempFile = File.createTempFile("ssl-key", ".p12");
+        @Cleanup FileOutputStream fos = new FileOutputStream(tempFile);
+        fos.write(keyFile.getBody());
+
+        return tempFile;
     }
 
     private Connector redirectConnector() {
--- src/main/java/com/github/dmn1k/tfm/security/SecurityConfig.java
+++ src/main/java/com/github/dmn1k/tfm/security/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private CustomUserDetailsService userDetailsService;
 
-    @Value("${server.ssl.enabled}")
+    @Value("${ssl.required}")
     private boolean sslRequired;
 
     @Bean
