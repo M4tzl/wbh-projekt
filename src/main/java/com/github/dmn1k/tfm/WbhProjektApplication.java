d58bfafc3a3e315ce85b1d8f220b802aae65d4b0
dmn1k <dominik.schlosser@gmail.com>
2018-09-16 19:38:17 +0200

    add etagfilter

--- src/main/java/com/github/dmn1k/tfm/WbhProjektApplication.java
+++ src/main/java/com/github/dmn1k/tfm/WbhProjektApplication.java
@@ -2,11 +2,20 @@ package com.github.dmn1k.tfm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+import javax.servlet.Filter;
 
 @SpringBootApplication
 public class WbhProjektApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WbhProjektApplication.class, args);
+    }
+
+    @Bean
+    public Filter shallowETagHeaderFilter() {
+        return new ShallowEtagHeaderFilter();
     }
 }
